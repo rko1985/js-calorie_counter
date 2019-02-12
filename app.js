cdb0922fb14bbaec77938cfe62c8dce0a1ee21e6
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-02-12 13:24:49 -1000

    updating items and total calories

--- app.js
+++ app.js
@@ -56,6 +56,21 @@ const ItemCtrl = (function(){
             });
             return found;
         },
+        updateItem: function(name, calories){
+            //calories to number
+            calories = parseInt(calories);
+
+            let found = null;
+
+            data.items.forEach(function(item){
+                if(item.id === data.currentItem.id){
+                    item.name = name;
+                    item.calories = calories;
+                    found = item;                    
+                }
+            });
+            return found;
+        },
         getTotalCalories: function(){
             let total = 0;
 
@@ -88,6 +103,7 @@ const UICtrl = (function(){
 
     const UISelectors = {
         itemList: '#item-list',
+        listItems: '#item-list li',
         addBtn: '.add-btn',
         updateBtn: '.update-btn',
         deleteBtn: '.delete-btn',
@@ -138,6 +154,22 @@ const UICtrl = (function(){
             //insert item
             document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li);
         },
+        updateListItem: function(item){
+            let listItems = document.querySelectorAll(UISelectors.listItems);
+            //turn node list into array
+            listItems = Array.from(listItems);
+
+            listItems.forEach(function(listItem){
+                const itemID = listItem.getAttribute('id');
+
+                if(itemID === `item-${item.id}`){
+                    document.querySelector(`#${itemID}`).innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                    <a href="#" class="secondary-content">
+                        <i class="edit-item fa fa-pencil"></i>
+                    </a>`;
+                }
+            });
+        },
         clearInput: function(){
             document.querySelector(UISelectors.itemNameInput).value = '';
             document.querySelector(UISelectors.itemCaloriesInput).value = '';
@@ -183,8 +215,18 @@ const App = (function(ItemCtrl, UICtrl){
 
         //Add item event
         document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+        //disable submit on enter
+        document.addEventListener('keypress', function(e){
+            if(e.keyCode === 13 || e.which === 13){
+                e.preventDefault();
+                return false;
+            }
+        });
         //edit icon click event
-        document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit);
+        document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick);
+        //update item event
+        document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit);
+
     }
 
     //Add item submit
@@ -209,8 +251,8 @@ const App = (function(ItemCtrl, UICtrl){
         e.preventDefault()
     }
 
-    //Update item submit
-    const itemUpdateSubmit = function(e){
+    //Click edit item
+    const itemEditClick = function(e){
         if(e.target.classList.contains('edit-item')){
             //get list item id (item-0 item-1)
             const listId = e.target.parentNode.parentNode.id;
@@ -227,6 +269,24 @@ const App = (function(ItemCtrl, UICtrl){
             UICtrl.addItemToForm();
         }
 
+        e.preventDefault();
+    }
+
+    //update item submit
+    const itemUpdateSubmit = function(e){
+        //get item input
+        const input = UICtrl.getItemInput();
+        //update item
+        const updatedItem = ItemCtrl.updateItem(input.name, input.calories);
+        //update UI
+        UICtrl.updateListItem(updatedItem);
+        //get total calories
+        const totalCalories = ItemCtrl.getTotalCalories();
+        //add total calorires to UI
+        UICtrl.showTotalCalories(totalCalories);
+
+        UICtrl.clearEditState();
+        
         e.preventDefault();
     }
     
